4ffb5684edbc024ab58fac7d1f63171fc8f2079d
Quinn Cox <qrc.inpa@gmail.com>
2019-02-27 00:40:16 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+  new_array = []
+  return new_array
+end
+
+
+def array_with_two_elements
+  my_two_array = ["element_one", "element_two"]
+  return my_two_array
+end
+
+
+def first_element(my_first_element)
+  return my_first_element[0]
+end
+
+
+def third_element(my_third_element)
+  return my_third_element[2]
+end
+
+
+def last_element(my_last_element)
+  return my_last_element[-1]
+end
+
+
+def first_element_with_array_methods(first_country)
+ return first_country.first
+end
+
+
+def last_element_with_array_methods(last_country)
+ return last_country.last
+end
+
+
+def length_of_array(length)
+  return length.length
+end
